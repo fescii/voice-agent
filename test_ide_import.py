c7c0419c2009e93b05b2c8e67682e6668e0c444c
fescii <isfescii@gmail.com>
2025-06-05 23:06:03 +0300

    chore: Remove obsolete test scripts for database, WebSocket, and import validation

--- test_ide_import.py
+++
@@ -1,8 +0,0 @@
-# Test file for IDE imports
-from services.ringover import RingoverStreamingService
-
-
-def test_function():
-  # Create an instance of the service
-  service = RingoverStreamingService()
-  return service
